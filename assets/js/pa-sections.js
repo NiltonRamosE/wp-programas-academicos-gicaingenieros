03599ccb822f987ba714ea00e0293fe9b985acf1
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-04-03 17:21:09 -0500

    feat: add root variables and active sections

--- assets/js/pa-sections.js
+++ assets/js/pa-sections.js
@@ -2,10 +2,26 @@ jQuery(document).ready(function($) {
     const navButtons = $('.pa-nav__btn');
     const programasSections = $('.programas-academicos-section');
 
+    function setInitialActiveButton() {
+        const visibleSection = programasSections.filter(':visible');
+        if (visibleSection.length) {
+            const activeCategory = visibleSection.attr('id');
+            navButtons.removeClass('pa-nav__btn--active')
+                     .filter(`[data-program-category="${activeCategory}"]`)
+                     .addClass('pa-nav__btn--active');
+        }
+    }
+
     navButtons.on('click', function() {
         const programCategory = $(this).data('program-category');
+        
+        navButtons.removeClass('pa-nav__btn--active');
+        $(this).addClass('pa-nav__btn--active');
+        
         programasSections.each(function() {
             $(this).toggle($(this).attr('id') === programCategory);
         });
     });
+
+    setInitialActiveButton();
 });
