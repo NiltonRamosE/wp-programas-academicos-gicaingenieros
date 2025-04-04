14642d9296caa9d5fd115625a873a406708ecfd4
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-04-04 10:29:47 -0500

    chore: enqueue style admin refactor

--- pa-gica-enqueue.php
+++ pa-gica-enqueue.php
@@ -1,26 +1,17 @@
 <?php
 
-// Encolar estilos del dashboard (ADMIN)
 function gica_enqueue_admin_styles($hook) {
     if ($hook == 'toplevel_page_gica-dashboard') {
-        wp_enqueue_style(
-            'gica-dashboard-style',
-            plugin_dir_url(__FILE__) . 'assets/dashboard.css',
-            array(),
-            '1.1.0',
-            'all'
-        );
+        wp_enqueue_style('gica-dashboard-style', plugin_dir_url(__FILE__) . 'assets/dashboard.css');
     }
 }
 add_action('admin_enqueue_scripts', 'gica_enqueue_admin_styles');
 
-// Encolar estilos del plugin
 function gica_enqueue_styles() {
     wp_enqueue_style('gica-programas-style', plugin_dir_url(__FILE__) . 'assets/style.css');
 }
 add_action('wp_enqueue_scripts', 'gica_enqueue_styles');
 
-// Encolar el archivo JavaScript
 function gica_enqueue_scripts() {
     wp_enqueue_script('jquery');
     wp_enqueue_script('gica-pa-script-sections', plugin_dir_url(__FILE__) . 'assets/js/pa-sections.js', array('jquery'), null, true);
