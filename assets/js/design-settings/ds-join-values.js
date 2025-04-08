7a9683e08d83c1f8fd29ac6c85f9ac582a710180
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-04-08 11:47:29 -0500

    optimize code for all situations

--- assets/js/design-settings/ds-join-values.js
+++ assets/js/design-settings/ds-join-values.js
@@ -1,22 +1,27 @@
 jQuery(document).ready(function($) {
-    $('.main-title__size-input-group').each(function() {
-        var $group = $(this);
-        var $tempInput = $group.find('.main-title__input--number');
-        var $unitSelect = $group.find('.main-title__unit-select');
-        var $realInput = $group.find('input[type="hidden"]');
-        
-        function updateField() {
-            $realInput.val($tempInput.val() + $unitSelect.val());
-        }
-        
-        $tempInput.on('input', updateField);
-        $unitSelect.on('change', updateField);
-        
-        var initialValue = $realInput.val();
-        var numberPart = initialValue.replace(/[^0-9.]/g, '');
-        var unitPart = initialValue.replace(/[0-9.]/g, '');
-        
-        $tempInput.val(numberPart);
-        $unitSelect.val(unitPart);
-    });
+    function setupSizeInputGroup(selector, inputClass, unitClass) {
+        $(selector).each(function() {
+            var $group = $(this);
+            var $tempInput = $group.find(inputClass);
+            var $unitSelect = $group.find(unitClass);
+            var $realInput = $group.find('input[type="hidden"]');
+
+            function updateField() {
+                $realInput.val($tempInput.val() + $unitSelect.val());
+            }
+
+            $tempInput.on('input', updateField);
+            $unitSelect.on('change', updateField);
+
+            var initialValue = $realInput.val();
+            var numberPart = initialValue.replace(/[^0-9.]/g, '');
+            var unitPart = initialValue.replace(/[0-9.]/g, '');
+
+            $tempInput.val(numberPart);
+            $unitSelect.val(unitPart);
+        });
+    }
+
+    setupSizeInputGroup('.main-title__size-input-group', '.main-title__input--number', '.main-title__unit-select');
+    setupSizeInputGroup('.navbar__size-input-group', '.navbar__input--number', '.navbar__unit-select');
 });
