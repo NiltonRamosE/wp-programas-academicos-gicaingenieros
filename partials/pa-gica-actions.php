9de26eb704b4da7462207e8792d340fbdada339e
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-04-10 18:14:11 -0500

    change buttons

--- partials/pa-gica-actions.php
+++ partials/pa-gica-actions.php
@@ -4,11 +4,26 @@
         <a href="<?php echo admin_url($redirect_page); ?>" class="gica-academic-program__action-btn gica-academic-program__action-btn--primary">
             <?php echo $redirect_page_name; ?>
         </a>
-        <button class="gica-academic-program__action-btn gica-academic-program__action-btn--secondary">
+        <a href="https://niltonramosencarnacion.vercel.app" target="_blank" class="gica-academic-program__action-btn gica-academic-program__action-btn--secondary">
             Ver Documentación
-        </button>
+        </a>
+        <a href="<?php echo admin_url($third_option_redirect_page); ?>" title="Exporta lo guardado en un archivo .json" class="gica-academic-program__action-btn gica-academic-program__action-btn--secondary">
+            <span class="dashicons dashicons-download"></span> <?php echo $third_option_name; ?>
+        </a>
+        <?php if(isset($fourth_option_name_add_program)): ?>
+        <form method="post" enctype="multipart/form-data" action="<?php echo admin_url('admin-post.php'); ?>">
+            <?php wp_nonce_field('import_academic_programs_nonce'); ?>
+            <input type="hidden" name="action" value="import_academic_programs">
+            <input type="file" name="import_json" accept=".json" required>
+            <button type="submit" class="gica-academic-program__action-btn gica-academic-program__action-btn--secondary">
+                <span class="dashicons dashicons-update spin"></span> <?php echo $fourth_option_name_add_program; ?>
+            </button>
+        </form>
+        <?php endif; ?>
+        <?php if(isset($fourth_option_name_design_settings)): ?>
         <button class="gica-academic-program__action-btn gica-academic-program__action-btn--secondary">
-            <?php echo $third_option_name; ?>
+            <span class="dashicons dashicons-update spin"></span> <?php echo $fourth_option_name_design_settings; ?>
         </button>
+        <?php endif; ?>
     </div>
 </section>
